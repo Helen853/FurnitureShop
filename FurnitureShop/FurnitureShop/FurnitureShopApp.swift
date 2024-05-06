78ad84088d1a47de78d1ff933fda7b9a65ea6081
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-06 15:10:35 +0500

    Сделан стартовый проект

---
+++ FurnitureShop/FurnitureShop/FurnitureShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopApp.swift
+//  FurnitureShop
+//
+//  Created by Киса Мурлыса on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
